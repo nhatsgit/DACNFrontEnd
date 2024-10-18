663bdd375f2db03a1e3cb411a916e8dc9b5a979b
Nguyen Anh Nhat <127674428+nhatsgit@users.noreply.github.com>
2024-10-18 23:04:38 +0700

    18/10

--- src/pages/User/ProductDetails/ProductDetails.js
+++ src/pages/User/ProductDetails/ProductDetails.js
@@ -1,105 +1,80 @@
-import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function ProductDetails() {
-    const { query } = useParams();
     return (
         <>
-            <div class="product-details">
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 padding-right">
+                            <div className="product-details">
+                                <div className="col-sm-5">
+                                    <div className="view-product">
+                                        <img src="images/product-details/product.png" alt="" />
+                                    </div>
+                                    <div id="similar-product" className="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="item active">
+                                                <a href=""><img src="images/product-details/similar2.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar3.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar2.jpg" width={125} height={125} alt="" /></a>
+                                            </div>
+                                            <div class="item">
+                                                <a href=""><img src="images/product-details/similar3.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar1.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar3.jpg" width={125} height={125} alt="" /></a>
+                                            </div>
+                                            <div class="item">
+                                                <a href=""><img src="images/product-details/similar2.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar1.jpg" width={125} height={125} alt="" /></a>
+                                                <a href=""><img src="images/product-details/similar2.jpg" width={125} height={125} alt="" /></a>
+                                            </div>
 
-                <div class="col-sm-5">
-                    <div class="view-product">
-                        <img src="images/home/react.png" alt="" />
-                    </div>
-                    <div id="similar-product" class="carousel slide" data-ride="carousel">
+                                        </div>
 
+                                        <a className="left item-control" style={{ left: 0 }} href="#similar-product" data-slide="prev">
+                                            <i className="fa fa-angle-left"></i>
+                                        </a>
+                                        <a className="right item-control" href="#similar-product" data-slide="next">
+                                            <i className="fa fa-angle-right"></i>
+                                        </a>
+                                    </div>
 
-                        <div class="carousel-inner">
-                            {/* @for (int j = 0; j < ViewBag.productImages.Count; j += 3)
-                            {
-                                @if (first == true)
-                            {
-                                <div class="item active">
-                                    @for (int i = 0; i < ViewBag.productImages.Count; i++)
-                                    {
-                                if (i != (j + 3))
-                                    {
-                                        <a href=""><img src="@ViewBag.productImages[i].Url" width="125" height="125" alt=""></a>
-                                    }
-                                    else
-                                    {
-                                        first = false;
-                                    break;
-                                }
-                            }
                                 </div>
-                            }
-                            else
-                            {
-                                <div class="item">
-                                    @for (int i = j; i < ViewBag.productImages.Count; i++)
-                                    {
-                                if (i != (j + 3))
-                                    {
-                                        <a href=""><img src="@ViewBag.productImages[i].Url" width="125" height="125" alt=""></a>
-                                    }
-                                    else
-                                    {
-                                        first = false;
-                                    break;
-                                }
-                            }
+                                <div className="col-sm-7">
+                                    <div className="product-information" style={{
+                                        textAlign: "left"
+                                    }}>
+                                        <h2>TenSp</h2>
+                                        <p>Web ID:ProductId</p>
+                                        <p style={{ textDecoration: "line-through" }}>Giá Gốc</p>
+                                        <span>
+                                            <span> Giá:77777</span>
+
+                                            <form asp-controller="ShoppingCart" asp-action="AddToCart" asp-route-productId="ProductId">
+                                                <input id="txtQuantity" onchange="updateCount()" type="number" min="1" value="1" oninput="removeNegative()" name="quantity" />
+                                                <button type="submit" className="btn btn-fefault cart">
+                                                    <Link style={{ color: "white" }} onmouseover="this.style.color='black'" onmouseout="this.style.color='white'"><i className="fa fa-shopping-cart"></i>Bỏ vào giỏ</Link>
+                                                </button>
+                                            </form>
+
+                                        </span>
+
+                                        <p><b style={{ color: "red" }}>------------Đã hết hàng-----------</b></p>
+                                        <p><b>Số lượng còn:</b> 1231231</p>
+                                        <p><b>Mô tả:</b> 13213</p>
+                                        <p><b>Thông số:</b> 1221313</p>
+                                        <p><b>Hãng:</b> TenNhanHieu</p>
+                                        <p><b>Loại:</b> TenLoai</p>
+                                        <Link to="/"><img src="../myLayout/images/product-details/share.png" className="share img-responsive" alt="" /></Link>
+                                    </div>
                                 </div>
-                            }
-                } */}
+                            </div >
                         </div>
-
-                        <a class="left item-control" href="#similar-product" data-slide="prev">
-                            <i class="fa fa-angle-left"></i>
-                        </a>
-                        <a class="right item-control" href="#similar-product" data-slide="next">
-                            <i class="fa fa-angle-right"></i>
-                        </a>
-                    </div>
-
-                </div>
-                <div class="col-sm-7">
-                    <div class="product-information">
-
-                        <h2>TenSp</h2>
-                        <p>Web ID:ProductId</p>
-                        <p style={{ textDecoration: "line-through" }}>Giá Gốc</p>
-                        <span>
-                            <span> Giá:77777</span>
-                            {
-                                () => {
-                                    if (true) {
-                                        <form asp-controller="ShoppingCart" asp-action="AddToCart" asp-route-productId="ProductId">
-                                            <input id="txtQuantity" onchange="updateCount()" type="number" min="1" value="@count" oninput="removeNegative()" name="quantity" />
-                                            <button type="submit" class="btn btn-fefault cart">
-                                                <a style={{ color: "white" }} onmouseover="this.style.color='black'" onmouseout="this.style.color='white'"><i class="fa fa-shopping-cart"></i>Bỏ vào giỏ</a>
-                                            </button>
-                                        </form>
-                                    }
-                                }
-                            }
-                        </span>
-                        {
-                            () => {
-                                if (true) {
-                                    <p><b>Số lượng còn:</b> SoLuongCon</p>
-                                }
-                                else {
-                                    <p><b style={{ color: "red" }}>------------Đã hết hàng-----------</b></p>
-                                }
-                            }
-                        }
-                        <p><b>Mô tả:</b> Mota</p>
-                        <p><b>Thông số:</b> ThongSo</p>
-                        <p><b>Hãng:</b> TenNhanHieu</p>
-                        <p><b>Loại:</b> TenLoai</p>
-                        <a href=""><img src="../myLayout/images/product-details/share.png" class="share img-responsive" alt="" /></a>
                     </div>
                 </div>
-            </div >
+            </section>
+
         </>
     );
 }
